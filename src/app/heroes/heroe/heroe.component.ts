2e13ea33791895dfb53aff8c5c867bcf59e54ba8
Jhosep Guadalupe <jhosep.guadalupe>
2021-02-18 15:35:35 -0500

    creando un componente con angular7cli

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.html'],
 })
-export class HeroeComponent {}
+export class HeroeComponent {
+  public nombre: string = 'Iron Man ';
+  public edad: number = 33;
+
+  public get nombreCapitalize(): string {
+    return this.nombre.toUpperCase();
+  }
+
+  obtenerNombre(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarNombre(): void {
+    this.nombre = 'Spider-Man';
+  }
+  cambiarEdad(): void {
+    this.edad = 15;
+  }
+}
