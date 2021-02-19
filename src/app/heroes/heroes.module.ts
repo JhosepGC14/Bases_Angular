03e2c858a4a97edb2f8feeac5840709553effeee
Jhosep Guadalupe <jhosep.guadalupe>
2021-02-19 17:44:40 -0500

    se creo el modulo de contador y heroes

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//componentes
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+
+@NgModule({
+  // aqui van los componentes que va a leer el modulo 
+  declarations: [ListadoComponent, HeroeComponent],
+  //aqui van los componentes que se van a exportar y van a ser visibles en los demas modulos
+  exports: [ListadoComponent],
+  //aqui solo se importan modulos
+  imports: [CommonModule],
+})
+export class HeroesModule {}
--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent {
+  heores: string[] = ['Spider Man', 'Hawkeye', 'Hulk', 'Thor', 'Iron man'];
 
+  heroeBorrado: string = '';
+
+  deleteHeroe() {
+    this.heroeBorrado = this.heores.shift() || '';
+  }
 }
