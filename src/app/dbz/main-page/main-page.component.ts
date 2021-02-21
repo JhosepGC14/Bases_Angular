a654c6780b0270ab9920cc8e854508262d7275e3
Jhosep Guadalupe <jhosep.guadalupe>
2021-02-21 18:49:08 -0500

    deleting imports without used

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NewCharacter } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
